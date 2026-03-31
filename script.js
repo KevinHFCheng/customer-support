80f425a3a823a1d736e08b849df19baedff78662
KevinHFCheng <kevin.hf.cheng@gmail.com>
2026-03-31 18:27:39 +0800

    修正回來 .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // --- 請填入您的 GAS 部署網址 ---
-const GAS_WEB_APP_URL = 'https://script.google.com/macros/library/d/1Om01zBPBx0s27jLPFHHAHJIrSVmWTDya9z6HsG9qGPLZ5e3elVlYpRn8/5';
+const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxfwQFOWT4nivHMQPGcBBUiV07yr6LMT8TmPqB_y2_AcyNlz5q7nFSpagzk7bhrdude/exec';
 
 const regForm = document.getElementById('registrationForm');
 const chatToggle = document.getElementById('chat-toggle-btn');
